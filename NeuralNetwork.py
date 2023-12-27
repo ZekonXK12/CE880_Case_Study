c8a6c8569abccd8f13e4ba7a86050b32657394b3
ZekonXK12 <zekonjinge@gmail.com>
2023-12-27 17:45:12 +0000

    2023/12/27 traditional ML models

---
+++ NeuralNetwork.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP,self).__init__()
+
+    def forward(self,x):
+        output=x
+        return output
+
+    
